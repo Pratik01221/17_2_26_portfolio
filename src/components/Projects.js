46178d9bc18647c0bce5e53eb864d5d699e45f77
PRATIK GARAD <pratikgarad1221@gmail.com>
2026-02-17 17:03:31 +0530

    first commit--2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import project1 from "../assets/project1.png";
 import textcraft from "../assets/textcraft.png";
 import portfolio from "../assets/Portfolio.png";
+
 export default function Projects() {
   return (
     <section className="featured-projects">
@@ -9,29 +10,50 @@ export default function Projects() {
       <div className="projects-grid">
 
         <div className="project-box">
-          <img src={textcraft} alt="Cover Letter AI" />
+          <img src={textcraft} alt="TextCraft Project" />
 
           <div className="project-info">
             <h3>TextCraft – Text Utility Web Application</h3>
-            <a href="https://textcraft-ruby.vercel.app/" target="_blank">Live Demo</a>
+            <a 
+              href="https://textcraft-ruby.vercel.app/" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>
           </div>
         </div>
 
         <div className="project-box">
-          <img src={portfolio} alt="Portfolio" />
+          <img src={portfolio} alt="Portfolio Project" />
 
           <div className="project-info">
             <h3>Personal Developer Portfolio Website</h3>
-            <a href="/" target="_blank">Live Demo</a>
+            <a 
+              href="/" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>
           </div>
         </div>
 
         <div className="project-box">
-          <img src={project1} alt="NewsFlash – Real-Time News Web Application" />
+          <img 
+            src={project1} 
+            alt="NewsFlash – Real-Time News Web Application" 
+          />
 
           <div className="project-info">
             <h3>NewsFlash – Real-Time News Web Application</h3>
-            <a href="https://news-flash-know-more-scroll-less.vercel.app/" target="_blank">Live Demo</a>
+            <a 
+              href="https://news-flash-know-more-scroll-less.vercel.app/" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              Live Demo
+            </a>
           </div>
         </div>
 
@@ -39,5 +61,3 @@ export default function Projects() {
     </section>
   );
 }
-
-
